22b01f1d0a1175672457a520e6aa3b581e9711fe
Kyle Latino <kyle.latino@gmail.com>
2017-06-19 17:44:40 -0400

    setup basic swift template

---
+++ template.swift
@@ -0,0 +1,19 @@
+/*
+Template file for creating swift scripts
+generated 19 June 2017
+
+*/
+
+// imports
+
+
+// global vars
+var text: String = "Hello world!"
+
+// functions
+
+
+// do stuff
+
+print(text)
+
